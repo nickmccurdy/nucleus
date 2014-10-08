450c2acec1b00e590db82e52f0aeb2e4c2a664ad
Nicolas McCurdy <thenickperson@gmail.com>
2014-10-08 12:44:49 -0400

    Add validations for bins and display JSON data in code tags

--- app/models/bin.rb
+++ app/models/bin.rb
@@ -1,2 +1,18 @@
+require 'json'
+
 class Bin < ActiveRecord::Base
+  validates :title, presence: true
+  validate :json_body
+
+  private
+
+  def json?(str)
+    !JSON.parse(str).nil?
+  rescue JSON::ParserError
+    false
+  end
+
+  def json_body
+    errors.add(:body, 'must be valid JSON') unless json? body
+  end
 end
