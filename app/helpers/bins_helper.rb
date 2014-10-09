5819bc8aeb71b530bd1bcc205c21c4fe68fda9de
Nicolas McCurdy <thenickperson@gmail.com>
2014-10-09 18:11:19 -0400

    Add basic syntax highlighting support for JSON

--- app/helpers/bins_helper.rb
+++ app/helpers/bins_helper.rb
@@ -1,2 +1,9 @@
+require 'rouge'
+
 module BinsHelper
+  def highlight(source)
+    formatter = Rouge::Formatters::HTML.new(inline_theme: 'github')
+    lexer = Rouge::Lexers::JSON.new
+    formatter.format(lexer.lex(source)).html_safe
+  end
 end
